169b56de8e9ce77ec7a6169b9f397cd302f6f0b1
Mariaa97-rodri <maria.aguilar@peopleshores.com>
2026-02-19 14:32:44 -0500

    lab 7 commit

---
+++ lessons_7/labs/react-movie-search/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  const apiKey = "98e3fb1f";
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // Load a default movie on mount
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
+}
